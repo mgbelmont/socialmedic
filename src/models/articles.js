ee2cc87711592a1a59e017561ddce61377018ccf
Mary <oxesescom@gmail.com>
2021-06-11 01:43:49 -0500

    update model articles

--- src/models/articles.js
+++ src/models/articles.js
@@ -1,7 +1,22 @@
 const moongose = require('mongoose')
 
 const articlesSchema = new moongose.Schema({
-    
+    title: {
+        type: String,
+        minLength: 2,
+        maxLength: 100,
+        required: true
+    },
+    content:{
+        type: String,
+        minLength:2,
+        required: true
+    },
+    image:{
+        type: String,
+        minLength: 2,
+        required: true
+    }
 })
 
 const model = mongoose.model('articles', articlesSchema)
