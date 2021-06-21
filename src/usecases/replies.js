17112b6a679d511c024bea5cf5edd90dfe150052
Beto Barrera <betomonter@gmail.com>
2021-06-21 01:27:05 -0500

    add usescase from replies

---
+++ src/usecases/replies.js
@@ -0,0 +1,25 @@
+
+const Replies = require('../models/replies');
+
+function getAll () {
+    return Replies.find();
+}
+
+function getByDocumentId(type,id){
+    return Replies.find({document_type:type,document_id:id}).sort({creationDate:-1})
+    .populate({path: "user_id", model: 'users',  })
+    .exec(); 
+}
+
+function create({ comment, document_type, document_id, user_id }){
+    const creationdate = Date.now();
+    return Replies.create({ comment, document_type, document_id, user_id, creationdate });
+}
+
+
+module.exports={
+    getAll,
+    getByDocumentId,
+    create
+}
+
