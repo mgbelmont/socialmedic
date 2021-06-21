f7c0cf9b8e3a7ffd004de0e04ae84cf30cd58669
Beto Barrera <betomonter@gmail.com>
2021-06-21 01:27:43 -0500

    add routers from replies

---
+++ src/routers/replies.js
@@ -0,0 +1,72 @@
+
+const express = require('express');
+const router = express.Router();
+const replies = require('../usecases/replies');
+
+router.get('/',  async (request,response)=>{
+    try{
+        const allReplies= await replies.getAll();
+        response.json({ 
+            success: true,
+            message: 'All replies',
+            data: {
+                replies: allReplies
+            }
+        });
+
+    }catch(error){
+        response.status(400);
+        response.json({ 
+            success: false,
+            message: 'Error at get all replies',
+            error:  error.message
+            
+        });
+    }    
+})
+
+router.post('/', async (request,response)=>{
+    try{
+        const replyCreated = await replies.create(request.body);
+        response.json({ 
+            success: true,
+            message: 'New Reply created :D',
+            data: {
+                replies: replyCreated
+            }
+        });
+
+    }catch(error){
+        response.status(400);
+        response.json({ 
+            success: false,
+            message: 'Error at Reply creation',
+            error:  error.message
+            
+        });
+    }    
+});
+
+router.get('/:type/:id',async (request,response)=>{
+    try{
+        const { type,id } = request.params;
+        const repliesByDocumentId = await replies.getByDocumentId(type,id);
+        response.json({ 
+            success: true,
+            message: 'Reply Found',
+            data: {
+                replies: repliesByDocumentId            }
+        });
+
+    }catch(error){
+        response.status(400)
+        response.json({ 
+            success: false,
+            message: 'Error at found replies',
+            error:  error.message
+            
+        });
+    }    
+});
+
+module.exports = router
