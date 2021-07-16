9311bee8c4bb2aa2b5ad8b3c642bdfb54033a0de
MarianaLimon <marianalimong@hotmail.com>
2021-07-16 01:12:09 -0500

    Add themplate mails

--- src/lib/sendEmail.js
+++ src/lib/sendEmail.js
@@ -25,21 +25,20 @@ async function sendEmail(email, type) {
     let htmlEmail = ""
     if (type === "Validando") {
         subjectEmail = "Registro Exitoso";
-        textEmail = "Le recordamos que ahora forma parte de SocialMedic donde podrá obtener la información más actualizada de la industria médica.Su cuenta será validada en unos momentos y recibirá un aviso por este mismo medio."
-        htmlEmail = "<p>Le recordamos que ahora forma parte de SocialMedic donde podrá obtener la información más actualizada de la industria médica.</p> <p>Su cuenta será validada en unos momentos y recibirá un aviso por este mismo medio.</p>"
-
+        textEmail = "¡Gracias por registrarse en SocialMedic!. Sus datos estan siendo validados. En breve recibirá un aviso por este medio, con el estatus de la aprobación de su cuenta. ¡Nos complace contar con su participación!"
+        htmlEmail = "<div><div style='background-color: #3C58C1; height: 120px; display: flex; flex-direction: column; align-items: center; '><img src='https://socialmedic-bucket.s3.us-east-2.amazonaws.com/logo.png' alt='' style='display: block; margin: auto; width: 300px;'></div><div style='margin: 5.2rem 3rem;'><h1 style='text-align: center; color: #53D2D3; font-family:Arial, Helvetica, sans-serif; font-size: 34px; margin-bottom: 4rem;'>¡Gracias por registrarse en SocialMedic!</h1><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 18px;'>Sus datos estan siendo validados.</p><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 18px; margin-bottom: 4rem;'>En breve recibirá un aviso por este medio, con el estatus de la aprobación de su cuenta.</p><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 22px;'><b>¡Nos complace contar con su participación!</b></p></div><div style='background-color: #CBE8F6; height: 80px;'></div></div>"
     }
 
     if (type === "Suscrito") {
         subjectEmail = "Cuenta Validada";
-        textEmail = "Su cuenta ha sido aprobada con éxito. Para iniciar sesión  visite http://socialmedic.com ¡Nos alegra contar con usted!"
-        htmlEmail = "<p>Su cuenta ha sido aprobada con éxito.</p><p>Para iniciar sesión  visite <a href='http://localhost:3000'>http://socialmedic.com</a></p><p>¡Nos alegra contar con usted!</p>"
+        textEmail = "¡Bienvenid@ a SocialMedic! Su cuenta ha sido aprobada. Para iniciar sesión  visite http://socialmedic.com ¡Nos complace contar con su participación!"
+        htmlEmail = "<div><div style='background-color: #3C58C1; height: 120px; display: flex; flex-direction: column; align-items: center; '><img src='https://socialmedic-bucket.s3.us-east-2.amazonaws.com/logo.png' alt='' style='display: block; margin: auto; width: 300px;'></div><div style='margin: 5.2rem 3rem;'><h1 style='text-align: center; color: #53D2D3; font-family:Arial, Helvetica, sans-serif; font-size: 34px; margin-bottom: 4rem;'>¡Bienvenid@ a SocialMedic!</h1><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 18px;'>Su cuenta ha sido <span style='color: #3C58C1;'>aprobada</span>.</p><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 18px; margin-bottom: 4rem;'>Para iniciar sesión  visite <a href='http://localhost:3000'>http://socialmedic.com</a></p><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 22px;'><b>¡Nos complace contar con su participación!</b></p></div><div style='background-color: #CBE8F6; height: 80px;'></div></div>"
     }
 
     if (type === "Rechazado") {
         subjectEmail = "Cuenta Rechazada";
-        textEmail = "Su cuenta ha sido rechazada debido a que los datos que fueron proporcionados no son válidos"
-        htmlEmail = "<p>Su cuenta ha sido rechazada debido a que los datos que fueron proporcionados no son válidos</p>"
+        textEmail = "¡Gracias por su registro! Su cuenta ha sido rechazada debido a que los datos que fueron proporcionados no son válidos. ¡Nos complace contar con su participación! "
+        htmlEmail = "<div><div style='background-color: #3C58C1; height: 120px; display: flex; flex-direction: column; align-items: center; '><img src='https://socialmedic-bucket.s3.us-east-2.amazonaws.com/logo.png' alt='' style='display: block; margin: auto; width: 300px;'></div><div style='margin: 5.2rem 3rem;'><h1 style='text-align: center; color: #53D2D3; font-family:Arial, Helvetica, sans-serif; font-size: 34px; margin-bottom: 4rem;'>¡Gracias por su registro!</h1><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 18px;'>Su cuenta ha sido <span style='color: red;'>rechazada</span> debido a que los datos que fueron proporcionados no son válidos.</p><p style='text-align: center; color: #6C6A6A; font-family:Arial, Helvetica, sans-serif; font-size: 22px;'><b>¡Nos complace contar con su participación!</b></p></div><div style='background-color: #CBE8F6; height: 80px;'></div></div>"
     }
 
 
